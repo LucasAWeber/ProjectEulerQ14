5b0a4ba9e06a1a97cb00ec3204ce17b63455a81a
Lucas Weber <lweber07@uoguelph.ca>
2022-02-07 21:02:05 -0500

    added cache to keep track of previous numbers' sequence length to reduce runtime

--- ProjectEulerQ14.c
+++ ProjectEulerQ14.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// can run just fine anything from 100000 and below but takes too long above that threshold
 #define UPPERBOUND 1000000
 
 int main()
 {
     int max = 0;
     int maxnum = 0;
-    int currentnum = 0;
+    long long int currentnum = 0;
     int current = 0;
+    // uses cashe to save the numbers' sequence length so once currentnum goes below the i it exits while loop and adds the current counter with the previously saved sequence length in the cache to reduce run time
+    int *cache;
+    cache = malloc(sizeof(int)*UPPERBOUND);
+    for (int i = 0; i < UPPERBOUND; i++)
+    {
+        cache[i] = -1;
+    }
 
     for (int i = 2; i < UPPERBOUND; i++)
     {
         currentnum = i;
         current = 0;
-        while (currentnum != 1)
+        while (currentnum != 1 && currentnum >= i)
         {
+            current++;
             if (currentnum % 2 == 0)
             {
                 currentnum /= 2;
@@ -24,16 +32,16 @@ int main()
             {
                 currentnum = 3 * currentnum + 1;
             }
-            current++;
         }
-        if (current > max)
+        cache[i] = current + cache[currentnum];
+        if (cache[i] > max)
         {
             maxnum = i;
-            max = current;
+            max = cache[i];
         }
     }
 
-    printf("%d", maxnum);
+    printf("%d\n", maxnum);
 
     return 1;
 }
